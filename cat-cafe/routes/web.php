bf4cbce71a4780bf9f215f7a50f5c049c5ca5972
y-ksmh0721 <kov.0nce.again@gmail.com>
2025-01-27 14:05:59 +0900

    controller作成

--- cat-cafe/routes/web.php
+++ cat-cafe/routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('contact',[ContactController::class, 'index']);
+Route::get('complete',[ContactController::class, 'complete']);
